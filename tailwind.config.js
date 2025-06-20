25eb48d1de07a52dba3ed7958dd8a40966d73c84
s-battle11 <Eedbattle@gmail.com>
2025-06-20 02:51:10 -0400

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+module.exports = {
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  theme: {
+    extend: {
+      colors: {
+        primary: "#047857",
+        secondary: "#f3f4f6",
+        pa: "#3B82F6",
+        ny: "#F59E0B",
+        mi: "#065F46",
+        oh: "#DC2626"
+      }
+    }
+  },
+  plugins: []
+}
